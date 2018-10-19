67b1d9c7573bea0d32f3b0a92826c383874d9cce
malik <mdmalik.b@gmail.com>
2018-10-19 21:08:50 +0800

    added expensetotal

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-export const  ExpenseList= (props) => (
+export const ExpenseList= (props) => (
     <div>
         {
             props.expenses.length === 0 ? (
